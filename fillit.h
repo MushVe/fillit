3171c2cc17c64f8a1d87b91e0064ea2eaf669a11
Clement ZHANG <czhang@student.42.fr>
2019-02-12 18:16:01 +0100

    tetrimino.c coord.c fillit.h

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: czhang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/09 15:11:39 by czhang            #+#    #+#             */
-/*   Updated: 2019/02/11 18:12:25 by czhang           ###   ########.fr       */
+/*   Updated: 2019/02/12 17:41:24 by czhang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,29 @@
 
 # include "libft/libft.h"
 
-int		is_valid_char(char c, int i);
+typedef struct	tetrimino
+{
+	int[4][2]			coord;
+	struct tetrimino	*next;
+	struct tetrimino	*previous;
+}				tetrimino_list;
 
-int		count_sharp(char *str);
+int			is_valid_char(char c, int i);
 
-int		neighbors(char *str);
+int			count_sharp(char *str);
 
-int		is_tetrimino(char *str);
+int			neighbors(char *str);
+
+int			is_tetrimino(char *str);
+
+tetrimino	*new_tetrimino(int **cord);
+
+void		add_tetrimino(tetrimino **alst, tetrimino *new);
+
+void		del_tetrimino(tetrimino **alst);
+
+void		iter_tetrimino(tetrimino *lst, void (*f)(tetrimino *tetri));
+
+tetrimino	map_tetrimino(tetrimino *lst, tetrimino *(*f)(tetrimino *tetri));
 
 #endif
